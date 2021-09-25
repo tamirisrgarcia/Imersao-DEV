c0003e355a1216714ff585c5b87a622389b1261f
TAMIRIS REZENDE GARCIA COSTA <87827996+tamirisrgarcia@users.noreply.github.com>
2021-09-25 16:40:55 -0300

    Add files via upload

---
+++ Mentalista/Mentalista.js
@@ -0,0 +1,14 @@
+var numeroSecreto = parseInt(Math.random() * 11);
+
+function Chutar() {
+  var elementoResultado = document.getElementById("resultado");
+  var chute = parseInt(document.getElementById("valor").value);
+  console.log(chute);
+  if (chute == numeroSecreto) {
+    elementoResultado.innerHTML = "Você acertou!";
+  } else if (chute > 10 || chute < 0) {
+    elementoResultado.innerHTML = "Você deve escolher um número entre 0 e 10!";
+  } else {
+    elementoResultado.innerHTML = "Você errou, tente novamente!";
+  }
+}
